ea15165b74ab0be42028f383ce65eeb709a0e3c6
Emily Venuto <emily.venuto@gmail.com>
2017-10-24 13:18:05 -0400

    login works
    Need to add session ability

---
+++ login.php
@@ -0,0 +1,20 @@
+
+<?php
+require 'config.php';
+
+   $user = mysqli_real_escape_string($conn, $_POST['username']);
+   $pass = mysqli_real_escape_string($conn, $_POST['password']);
+
+   $query = mysqli_query($conn, "SELECT * FROM `Staff` WHERE `UserName` = '$user' AND `Password` = '$pass'") 
+	or die (mysqli_error()); 
+
+   if(!mysqli_num_rows($query))
+	echo "No results found.";
+   else {
+	header("Location: homepage.html");
+
+   }    
+   
+   mysqli_close($conn);
+
+?>
